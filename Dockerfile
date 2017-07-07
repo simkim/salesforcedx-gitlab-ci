9d11f9217cc11cc626011b271ca23c371bf7c006
Gauthier Monserand <simkim@simkim.net>
2017-07-07 23:29:48 +0200

    Initial WIP commit

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+FROM simkim/salesforcedx
+VOLUME /app
+# sfdx environment
+ENV SFDX_AUTOUPDATE_DISABLE true
+ENV SFDX_USE_GENERIC_UNIX_KEYCHAIN true
+ENV SFDX_DOMAIN_RETRY 300
+# build environment
+ENV CI_SFDX_ORG ciorg
+ENV CI_SFDX_KEY /app/assets/server.key
+ENV CI_SFDX_SCRATCH_DEF /app/config/project-scratch-def.json
+COPY ci-test.sh /usr/local/bin/
+CMD ci-test.sh
---
+++ ci-test.sh
@@ -0,0 +1,18 @@
+set -e
+set -x
+
+if [ -z $CI_SFDX_USERNAME ];then
+  echo "You must define CI_SFDX_USERNAME with your HubOrg username"
+  exit 1
+fi
+
+if [ ! -f $CI_SFDX_KEY ];then
+  echo "$CI_SFDX_KEY must be present"
+  exit 1
+fi
+
+sfdx force:auth:jwt:grant --clientid $CONSUMERKEY --jwtkeyfile $CI_SFDX_KEY --username $CI_SFDX_USERNAME --setdefaultdevhubusername -a HubOrg
+sfdx force:org:create -v HubOrg -s -f $CI_SFDX_SCRATCH_DEF -a $CI_SFDX_ORG
+sfdx force:source:push -u $CI_SFDX_ORG
+sfdx force:apex:test:run -u $CI_SFDX_ORG -c -r human
+sfdx force:org:delete -u $CI_SFDX_ORG -p
