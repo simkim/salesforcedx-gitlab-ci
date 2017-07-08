612593e1b6d053a1b674c90dfe92a6554eb40094
Gauthier Monserand <simkim@simkim.net>
2017-07-08 23:02:53 +0200

    Make CI_SFDX_* files relative to CI_PROJECT_DIR

--- Dockerfile
+++ Dockerfile
@@ -6,8 +6,9 @@ ENV SFDX_AUTOUPDATE_DISABLE true
 ENV SFDX_USE_GENERIC_UNIX_KEYCHAIN true
 ENV SFDX_DOMAIN_RETRY 300
 # build environment
+ENV CI_PROJECT_DIR /app
 ENV CI_SFDX_ORG ciorg
-ENV CI_SFDX_KEY /app/assets/server.key
-ENV CI_SFDX_SCRATCH_DEF /app/config/project-scratch-def.json
+ENV CI_SFDX_KEY assets/server.key
+ENV CI_SFDX_SCRATCH_DEF config/project-scratch-def.json
 COPY sfdx-test.sh /usr/local/bin/
 CMD bash
--- sfdx-test.sh
+++ sfdx-test.sh
@@ -7,13 +7,18 @@ if [ -z $CI_SFDX_USERNAME ];then
   exit 1
 fi
 
-if [ ! -f $CI_SFDX_KEY ];then
-  echo "$CI_SFDX_KEY must be present"
+if [ ! -f "$CI_PROJECT_DIR/$CI_SFDX_KEY" ];then
+  echo "$CI_PROJECT_DIR/$CI_SFDX_KEY must be present"
   exit 1
 fi
 
-sfdx force:auth:jwt:grant --clientid $CI_SFDX_CONSUMER_KEY --jwtkeyfile $CI_SFDX_KEY --username $CI_SFDX_USERNAME --setdefaultdevhubusername -a HubOrg
-sfdx force:org:create -v HubOrg -s -f $CI_SFDX_SCRATCH_DEF -a $CI_SFDX_ORG
+if [ ! -f "$CI_PROJECT_DIR/$CI_SFDX_SCRATCH_DEF" ];then
+  echo "$CI_PROJECT_DIR/$CI_SFDX_SCRATCH_DEF must be present"
+  exit 1
+fi
+
+sfdx force:auth:jwt:grant --clientid $CI_SFDX_CONSUMER_KEY --jwtkeyfile "$CI_PROJECT_DIR/$CI_SFDX_KEY" --username $CI_SFDX_USERNAME --setdefaultdevhubusername -a HubOrg
+sfdx force:org:create -v HubOrg -s -f "$CI_PROJECT_DIR/$CI_SFDX_SCRATCH_DEF" -a $CI_SFDX_ORG
 sfdx force:source:push -u $CI_SFDX_ORG
 sfdx force:apex:test:run -u $CI_SFDX_ORG -c -r human
 sfdx force:org:delete -u $CI_SFDX_ORG -p
